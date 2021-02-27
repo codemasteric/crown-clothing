495aad03bbc73191716bc63127d66f5bbcd88be8
codemasteric <onenfelix90@gmail.com>
2021-02-27 12:30:50 +0300

    adding header page and links to homepage, shop and contacts

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import MenuItem from '../menu-item/menu-item.component';
+import './directory.styles.scss';
+
+import SECTIONS_DATA from './sections.data.js';
+
+class Directory extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            sections: SECTIONS_DATA
+        }
+    }
+
+    render() {
+        return (
+            <div className="directory-menu" >
+                {this.state.sections.map(({ id, title, imageUrl, size, linkUrl }) => (
+                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl} />
+                )
+                )}
+            </div>
+        );
+    }
+}
+
+
+export default Directory;
